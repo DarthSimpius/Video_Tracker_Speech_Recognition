2b2a4b85e844e576501735115634c4b23bc46864
DarthSimpius <113943517+DarthSimpius@users.noreply.github.com>
2026-01-09 18:30:49 -0500

    Initial commit

---
+++ Speech_Checker.py
@@ -0,0 +1,55 @@
+import speech_recognition as speech
+import sys
+from difflib import SequenceMatcher
+sound = speech.Recognizer()
+
+with speech.Microphone() as audio:
+    print("Adjusting Energy Level")
+    sound.adjust_for_ambient_noise(audio)
+    sound.pause_threshold = 2
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+#     print("Say Something!")
+#     said = sound.listen(audio)
+
+# try:
+#     print(sound.recognize_google(said, language = 'en-IN', show_all = True))
+# except LookupError:
+#     print("Could not understand. Please repeat")
+# except speech.UnknownValueError:
+#     print("Could not understand. Please repeat")
+
+
+#Video_Name = sys.argv[0]
+#File_Path = "Video_Scripts\\" + Video_Name + "_Script.txt"
+File_Path = "Video_Transcriptions\\01_7L_Departure_North.txt"
+with open(File_Path, 'r') as f:
+    Dialogue_Lines = f.readlines()
+    f.close()
+    print(Dialogue_Lines)
+
+Finished_Dialogue = False
+Dialogue_Index = 0
+while not Finished_Dialogue:
+    if (Dialogue_Index != len(Dialogue_Lines)):
+        with speech.Microphone() as audio:
+            print("Say Something!")
+            said = sound.listen(audio)
+
+        try:
+            Spoken = sound.recognize_google(said)
+            print(Spoken)
+            if similar(Spoken,Dialogue_Lines[Dialogue_Index]) > 0.6:
+                Dialogue_Index += 1
+                print("Check Passed")
+            else:
+                print("Check not passed")
+        except LookupError:
+            print("Could not understand. Please repeat")
+        except speech.UnknownValueError:
+            print("Could not understand. Please repeat")
+    else:
+        Finished_Dialogue = True
+
+
